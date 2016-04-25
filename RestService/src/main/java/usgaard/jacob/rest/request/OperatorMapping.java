77f9096b9ec2152eb3d78650d3150a9f0cbae336
Jacob Usgaard <jacob.usgaard@outlook.com>
2016-04-24 19:38:20 -0500

    Adding OperatorMappings

---
+++ RestService/src/main/java/usgaard/jacob/rest/request/OperatorMapping.java
@@ -0,0 +1,33 @@
+package usgaard.jacob.rest.request;
+
+import java.util.regex.Pattern;
+
+import usgaard.jacob.rest.RestService.Operator;
+
+public class OperatorMapping {
+	private Pattern pattern;
+	private Operator operator;
+
+	public OperatorMapping(Pattern pattern, Operator operator) {
+		super();
+		this.pattern = pattern;
+		this.operator = operator;
+	}
+
+	public Pattern getPattern() {
+		return pattern;
+	}
+
+	public void setPattern(Pattern pattern) {
+		this.pattern = pattern;
+	}
+
+	public Operator getOperator() {
+		return operator;
+	}
+
+	public void setOperator(Operator operator) {
+		this.operator = operator;
+	}
+
+}
